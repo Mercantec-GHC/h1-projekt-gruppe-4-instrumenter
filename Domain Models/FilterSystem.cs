9cccb90759900c7efb35705c09e32cd9d71a1164
Elic0 <charlieoljac@gmail.com>
2023-11-14 10:18:08 +0100

    added another system

--- Domain Models/FilterSystem.cs
+++ Domain Models/FilterSystem.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Diagnostics.Metrics;
 
 namespace Objects
 {
     public class Filter
     {
         itemForSale itemForSale;
-        public void FilterInstrument()    
+        public void FilterInstrument()
         {
             Console.WriteLine(itemForSale);
         }
