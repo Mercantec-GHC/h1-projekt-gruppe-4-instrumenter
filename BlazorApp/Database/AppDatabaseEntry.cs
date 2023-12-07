e4eb3f44045d0b79b87827c268ddb3de976608fa
Elic0 <charlieoljac@gmail.com>
2023-12-07 13:44:20 +0100

    database connection complete

--- BlazorApp/Database/AppDatabaseEntry.cs
+++ BlazorApp/Database/AppDatabaseEntry.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlazorApp.Database
-{
-    public interface IDatabaseEntry
-    {
-        public IDatabaseEntry GetDBEntry(int pKey);
-        public void AddDBEntry();
-        public void RemoveDBEntry();
-        public bool UpdateDBEntry();
-    }
-}
+﻿
