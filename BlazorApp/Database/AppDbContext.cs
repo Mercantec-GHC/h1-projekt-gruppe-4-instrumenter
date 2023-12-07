263ad4d488ce5bdcff903f2595b511a113d5c0e6
Elic0 <charlieoljac@gmail.com>
2023-12-07 11:32:10 +0100

    complete redo

--- BlazorApp/Database/AppDbContext.cs
+++ BlazorApp/Database/AppDbContext.cs
@@ -7,7 +7,6 @@ using Microsoft.Data.SqlClient;
 
 namespace BlazorApp.Data
 {
-    public class AppDbContext { }
     public static class DatabaseHandler
     {
         private static SqlConnection _connection; //Ændre senere
