ecd343bf7503e67adcd4e18729f8ffccc288ba78
Elic0 <charlieoljac@gmail.com>
2023-12-11 12:21:20 +0100

    database output

---
+++ BlazorApp/Data/CustomerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorApp.Data
+{
+    public class CustomerInfo
+    {
+        public int CustomerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string Gender { get; set; }
+    }
+}
---
+++ BlazorApp/Data/CustomerService.cs
@@ -0,0 +1,37 @@
+﻿namespace BlazorApp.Data
+{
+    public class CustomerService
+    {
+        private readonly CustomerDataAccessLayer objCustomerDAL; // Declare objCustomerDAL
+
+        public CustomerService()
+        {
+            objCustomerDAL = new CustomerDataAccessLayer(); // Instantiate objCustomerDAL
+        }
+        public string Create(CustomerInfo objCustomer)
+        {
+            objCustomerDAL.AddCustomer(objCustomer);
+            return "Added Successfully";
+        }
+        public List<CustomerInfo> GetCustomer()
+        {
+            List<CustomerInfo> customers = objCustomerDAL.GetAllCustomers().ToList();
+            return customers;
+        }
+        public CustomerInfo GetCustomerByID(int id)
+        {
+            CustomerInfo customer = objCustomerDAL.GetCustomerData(id);
+            return customer;
+        }
+        public string UpdateCustomer(CustomerInfo objcustomer)
+        {
+            objCustomerDAL.UpdateCustomer(objcustomer);
+            return "Update Successfully";
+        }
+        public string DeleteCustomer(CustomerInfo objcustomer)
+        {
+            objCustomerDAL.DeleteCustomer(objcustomer.CustomerId);
+            return "Delete Successfully";
+        }
+    }
+}
