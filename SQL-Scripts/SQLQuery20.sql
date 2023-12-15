758b964ddda5eda5f1868b3c99b849f13740633a
Elic0 <charlieoljac@gmail.com>
2023-12-15 09:44:37 +0100

    more sql scripts

---
+++ SQL-Scripts/SQLQuery20.sql
@@ -0,0 +1,21 @@
+/****** Object:  Table [dbo].[listing]    Script Date: 12/15/2023 9:29:43 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[listing](
+	[name] [text] NULL,
+	[TypeInstrument] [text] NULL,
+	[Condition] [text] NULL,
+	[Price] [int] NULL,
+	[description] [text] NULL,
+	[color] [text] NULL,
+	[material] [text] NULL,
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Year] [int] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
