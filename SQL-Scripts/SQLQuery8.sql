cbe0ab956002072ced5b1a5e3c5a808401466d9e
Mape0009 <mape0009@edu.mercantec.dk>
2023-12-19 10:09:27 +0100

    SQL Query

---
+++ SQL-Scripts/SQLQuery8.sql
@@ -0,0 +1,18 @@
+-- Procedure to get instruments based on selected values
+CREATE PROCEDURE [dbo].[GetInstrumentsByPiano]
+    @InstrumentType NVARCHAR(MAX)
+AS   
+BEGIN   
+    SELECT
+        ID,
+        Name,
+        TypeInstrument,
+        Condition,
+        Price,
+        Description,
+        Year,
+        Color,
+        Material
+    FROM listing
+    WHERE TypeInstrument = 'Piano'
+END
